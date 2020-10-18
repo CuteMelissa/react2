08ad636fc4b89275313907244806915ea180d1a2
刘紫璐 <44942900+CuteMelissa@users.noreply.github.com>
2020-10-18 16:54:24 +0800

    Add files via upload

---
+++ precache-manifest.3a0c84c4c44782d67e788491c85df1ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1a150994c966625033972c5d31c0b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57954157d60a971861ae",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "45282df6e5f2fa0995df",
+    "url": "/static/css/main.c12616cd.chunk.css"
+  },
+  {
+    "revision": "57954157d60a971861ae",
+    "url": "/static/js/2.d72a010d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.d72a010d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45282df6e5f2fa0995df",
+    "url": "/static/js/main.c538b348.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
